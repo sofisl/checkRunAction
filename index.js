5314d1b899a9cef8ba82d0448ec07935b6ca4de6
Sofia Leon <sofialeon@google.com>
2020-07-28 17:44:19 -0700

    feat: initial stub of github action

---
+++ index.js
@@ -0,0 +1,32 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  // `who-to-greet` input defined in action metadata file
+  const conclusion = core.getInput('conclusion');
+  const author = core.getInput('author');
+  const myToken = core.getInput('myToken');
+  const octokit = github.getOctokit(myToken);
+  const checkRun;
+  if (conclusion === 'success' && author === 'sofisl') {
+    checkRun = await octokit.checks.create({
+      owner: 'sofisl',
+      repo: 'new2dis',
+      name: 'conditional check',
+      status: 'completed',
+      conclusion: 'success',
+    });
+  } else {
+    checkRun = await octokit.checks.create({
+      owner: 'sofisl',
+      repo: 'new2dis',
+      name: 'conditional check',
+      status: 'completed',
+      conclusion: 'failure',
+  })
+  }
+  core.setOutput("conclusion", checkRun);
+
+} catch (error) {
+  core.setFailed(error.message);
+}

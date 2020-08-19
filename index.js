3b4659b4967f73a0a4cdd31bff18d6fd8c1eb516
Sofia Leon <sofialeon@google.com>
2020-08-18 17:18:11 -0700

    fix: include missing awaits

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ async function getPRAuthor(owner, repo, head_sha, octokit) {
     repo,
     commit_sha: head_sha
   })).data[0].user.login;
+  console.log(`pr author: ${prAuthor}`);
   return prAuthor;
 }
 
@@ -19,32 +20,35 @@ async function createCheckRun(owner, repo, octokit, conclusion, head_sha, testNa
     status: 'completed',
     conclusion: conclusion,
     output: {
-      title: 'A conditional check run',
-      summary: 'Even though your required check did not pass, this will be fine'
+      title: `A conditional check run for ${testNameToCheckAgainst}`,
+      summary: 'This required check passed or failed depening on your summary_check.yml file in this repo'
     }    
   })
+  console.log(`check run created: ${createCheckRun}`);
   return checkRun;
 }
 
-
-try {
-  const owner = core.getInput('owner');
-  const repo = core.getInput('repo');
-  const testName = core.getInput('testName');
-  const testNameToCheckAgainst = core.getInput('testNameToCheckAgainst')
-  const conclusion = core.getInput('conclusion');
-  const myToken = core.getInput('myToken');
-  const octokit = github.getOctokit(myToken);
-  const testAuthorToCheckAgainst = core.getInput('testAuthorToCheckAgainst');
-  const head_sha = core.getInput('pull_request_head_sha');
-  const author = getPRAuthor(owner, repo, head_sha, octokit);
-  let checkRun;
-  if (conclusion !== 'success' && author === testAuthorToCheckAgainst && testName === testNameToCheckAgainst) {
-    checkRun = createCheckRun(owner, repo, octokit, 'failure', head_sha, testNameToCheckAgainst);
-  } else {
-    checkRun = createCheckRun(owner, repo, octokit, 'success', head_sha, testNameToCheckAgainst);
-   }
-  core.setOutput("conclusion", checkRun);
-} catch (error) {
-  core.setFailed(error.message);
+async function main() {
+  try {
+    const owner = core.getInput('owner');
+    const repo = core.getInput('repo');
+    const testName = core.getInput('testName');
+    const testNameToCheckAgainst = core.getInput('testNameToCheckAgainst')
+    const conclusion = core.getInput('conclusion');
+    const myToken = core.getInput('myToken');
+    const octokit = github.getOctokit(myToken);
+    const testAuthorToCheckAgainst = core.getInput('testAuthorToCheckAgainst');
+    const head_sha = core.getInput('pull_request_head_sha');
+    const author = await getPRAuthor(owner, repo, head_sha, octokit);
+    let checkRun;
+    if (conclusion !== 'success' && author === testAuthorToCheckAgainst && testName === testNameToCheckAgainst) {
+       checkRun = await createCheckRun(owner, repo, octokit, 'failure', head_sha, testNameToCheckAgainst);
+    } else {
+       checkRun = await createCheckRun(owner, repo, octokit, 'success', head_sha, testNameToCheckAgainst);     }
+    core.setOutput("conclusion", checkRun);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+main();
